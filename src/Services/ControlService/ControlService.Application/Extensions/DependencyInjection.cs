ae30abfa3baaf4fdbbe24ce2ebe1309761a418b2
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-07-07 13:40:53 +0300

    feat(control): implement elk logging

--- src/Services/ControlService/ControlService.Application/Extensions/DependencyInjection.cs
+++ src/Services/ControlService/ControlService.Application/Extensions/DependencyInjection.cs
@@ -16,6 +16,7 @@ public static class DependencyInjection
         {
             cfg.RegisterServicesFromAssembly(assembly);
 
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ConditionOwnsSensorBehavior<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(VacationModeSecurityBehavior<,>));
--- src/Services/ControlService/ControlService/Program.cs
+++ src/Services/ControlService/ControlService/Program.cs
@@ -1,21 +1,47 @@
 using Contracts.Middlewares;
 using Control.API.Extensions;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddConfiguration(builder.Configuration);
+try
+{
+    Log.Information("Starting ControlService application");
 
-WebApplication app = builder.Build();
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-app.UseGlobalExceptionHandler();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapHealthChecks(ApiConstants.HealthRoute);
-app.MapControllers();
+    builder.AddElkLogging();
 
-await app.RunAsync();
+    builder.Services.AddConfiguration(builder.Configuration);
+
+    WebApplication app = builder.Build();
+
+    app.UseGlobalExceptionHandler();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapHealthChecks(ApiConstants.HealthRoute);
+    app.MapControllers();
+
+    await app.RunAsync();
+}
+#pragma warning disable S2139
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "ControlService terminated unexpectedly");
+    throw;
+}
+#pragma warning restore S2139
+finally
+{
+#pragma warning disable S6966 
+    Log.CloseAndFlush();
+#pragma warning restore S6966 
+}
 
 #pragma warning disable S1118 
 public partial class Program { }
