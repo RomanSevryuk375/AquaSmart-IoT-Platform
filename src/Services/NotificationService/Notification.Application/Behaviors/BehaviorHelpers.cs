19298d5935c2f23f59c6712adc22a464929dc6ec
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-07-12 21:02:35 +0300

    fix: remove runtime reflection in behavior helpers

--- src/Services/NotificationService/Notification.Application/Behaviors/BehaviorHelpers.cs
+++ src/Services/NotificationService/Notification.Application/Behaviors/BehaviorHelpers.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using System.Reflection;
 using Contracts.Results;
 
@@ -5,27 +6,57 @@ namespace Notification.Application.Behaviors;
 
 public static class BehaviorHelpers
 {
-    public static TResponse CreateFailedResult<TResponse>(Error error)
+    public static TResponse CreateFailedResult<TResponse>(Error error) =>
+        ResultFactory<TResponse>.CreateFailure(error);
+
+    private static class ResultFactory<TResponse>
     {
-        if (typeof(TResponse) == typeof(Result))
-        {
-            return (TResponse)(object)Result.Failure(error);
-        }
+        public static readonly Func<Error, TResponse> CreateFailure;
 
-        if (typeof(TResponse).IsGenericType &&
-            typeof(TResponse).GetGenericTypeDefinition() == typeof(Result<>))
+#pragma warning disable S3963 
+        static ResultFactory()
+#pragma warning restore S3963 
         {
-            Type resultTypeArg = typeof(TResponse).GetGenericArguments()[0];
-            Type genericResultType = typeof(Result<>).MakeGenericType(resultTypeArg);
-            MethodInfo? failureMethod = genericResultType.GetMethod("Failure");
+            Type responseType = typeof(TResponse);
 
-            if (failureMethod != null)
+            if (responseType == typeof(Result))
             {
-                object? failedResult = failureMethod.Invoke(null, [error]);
-                return (TResponse)failedResult!;
+                CreateFailure = error => (TResponse)(object)Result.Failure(error);
+                return;
+            }
+
+            if (responseType.IsGenericType &&
+                responseType.GetGenericTypeDefinition() == typeof(Result<>))
+            {
+                Type resultTypeArg = responseType.GetGenericArguments()[0];
+                Type genericResultType = typeof(Result<>).MakeGenericType(resultTypeArg);
+
+                MethodInfo? failureMethod = genericResultType.GetMethod(
+                    "Failure",
+                    BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly,
+                    null,
+                    [typeof(Error)],
+                    null);
+
+                if (failureMethod is null)
+                {
+#pragma warning disable S3877 
+                    throw new InvalidOperationException($"Method 'Failure' not found on {genericResultType.Name}");
+#pragma warning restore S3877 
+                }
+
+                ParameterExpression errorParam = Expression.Parameter(typeof(Error), "error");
+                MethodCallExpression methodCall = Expression.Call(null, failureMethod, errorParam);
+                UnaryExpression castExpression = Expression.Convert(methodCall, typeof(TResponse));
+
+                CreateFailure = Expression.Lambda<Func<Error, TResponse>>(
+                    castExpression, errorParam).Compile();
+            }
+            else
+            {
+                CreateFailure = _ =>
+                    throw new UnauthorizedAccessException($"Unsupported response type: {responseType.Name}");
             }
         }
-
-        throw new UnauthorizedAccessException($"Access failure: {error.Message}");
     }
 }
